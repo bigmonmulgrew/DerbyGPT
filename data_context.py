a32823ff6676a721a127290c909413ac6b301cfd
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-01-16 11:03:51 +0000

    Added data context
    Added addition context and ability to import data into context from CSV format.
    Also added additional comments and reworded some of the context where data context was removed from the main context.

---
+++ data_context.py
@@ -0,0 +1,10 @@
+## Example data context
+csv_data = """
+Lecturer,        Email,                     Lecture,   Tutorial,    Subject
+Ovidiu Bagdasar, o.bagdasar@derby.ac.uk,    Mon 13-14, Thur 15-16,  Computational Mathematics
+Chris Windmill,  c.windmill@derby.ac.uk     Mon 10-11, Fri 11-13,   Foundations of Computer and It systems
+James Hardy,     j.hardy@derby.ac.uk,       Tue 13-14, Tue 14-16,   Fundamentals of Networks and Security
+Patrick Merrit,  p.merritt@derby.ac.uk      Mon 12-13, Thur 16-17,  Games Technologies
+Sam O'Neill,     s.oneill@derby.ac.uk,      Mon 9-10,  Fri 11-13,   Computer Systems Data Structures and Data Management
+Wayne Rippin,    w.rippin@derby.ac.uk,      ,          ,            Programme Leader
+"""
