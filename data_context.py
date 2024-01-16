1069c5803add11c723005eec2bc32e13c2fa1c61
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-01-16 11:41:07 +0000

    Added last semester courses to data

--- data_context.py
+++ data_context.py
@@ -6,5 +6,6 @@ Chris Windmill,  c.windmill@derby.ac.uk     Mon 10-11, Fri 11-13,   Foundations 
 James Hardy,     j.hardy@derby.ac.uk,       Tue 13-14, Tue 14-16,   Fundamentals of Networks and Security
 Patrick Merrit,  p.merritt@derby.ac.uk      Mon 12-13, Thur 16-17,  Games Technologies
 Sam O'Neill,     s.oneill@derby.ac.uk,      Mon 9-10,  Fri 11-13,   Computer Systems Data Structures and Data Management
+Sam O'Neill,     s.oneill@derby.ac.uk,      ,          ,            Programming
 Wayne Rippin,    w.rippin@derby.ac.uk,      ,          ,            Programme Leader
 """
