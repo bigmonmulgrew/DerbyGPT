b3997993bb1b0be3256664b95899a87951436b15
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-01-18 16:13:38 +0000

    Added new timing mechanism and multiple guilds
    Still need to cleanup old code after testing

--- utils.py
+++ utils.py
@@ -9,3 +9,7 @@ def set_debug_mode(mode):
 def debug(message):
     if DEBUG:
         print(message)
+
+def clamp(value, min_value, max_value):
+    """Ensure that 'value' is between 'min_value' and 'max_value'."""
+    return max(min_value, min(value, max_value))
